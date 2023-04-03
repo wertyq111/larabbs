afec4cb434b658506b12e8e1c84119294a03ce6d
zhouxufeng <wertyq222@163.com>
2023-04-02 21:55:38 -0500

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -14,4 +14,22 @@ class Topic extends Model
         'excerpt',
         'slug'
     ];
+
+    /**
+     * 一对一关联话题分类
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * 一对一关联用户
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
---
+++ resources/lang/zh_CN/topics.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'List' => '话题列表',
+    'Last Reply' => "最后回复",
+    'New Release' => "最新发布",
+    'Finally active in' => '最后活跃于',
+    'Right navigation bar' => '右边导航栏',
+];
