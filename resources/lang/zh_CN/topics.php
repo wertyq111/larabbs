0b5d767d8edd365d1e9cc2ffeab164575943b66b
zhouxufeng <wertyq222@163.com>
2023-04-04 01:15:32 -0500

    话题内容页面

--- resources/lang/zh_CN/topics.php
+++ resources/lang/zh_CN/topics.php
@@ -3,6 +3,7 @@ return [
     'List' => '话题列表',
     'Last Reply' => "最后回复",
     'New Release' => "最新发布",
+    'Author' => '作者',
     'Finally active in' => '最后活跃于',
     'Right navigation bar' => '右边导航栏',
     'Reply' => '回复',
